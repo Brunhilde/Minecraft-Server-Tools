1f47db3c89c4ba1f77c6155466f1275f588f78e1
Tobias Dieterich <tobidiet@gmx.de>
2011-06-08 19:51:04 +0200

    implemented cmake functionality and introduced some new classes

---
+++ UserManager.cpp
@@ -0,0 +1,14 @@
+#include <soci/soci.h>
+#include <soci/sqlite3/soci-sqlite3.h>
+
+#include "UserManager.hpp"
+
+using namespace soci;
+
+cUserManager::cUserManager()
+{
+}
+
+cUserManager::~cUserManager()
+{
+}
---
+++ UserManager.hpp
@@ -0,0 +1,13 @@
+#ifndef __USERMANAGER_HPP
+#define __USERMANAGER_HPP
+
+class cUserManager
+{
+public:
+	cUserManager();
+	~cUserManager();
+	
+private:
+};
+
+#endif // __USERMANAGER_HPP
